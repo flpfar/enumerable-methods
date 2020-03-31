8148cab5c893aee2f3e785a2275b55c828e40791
Felipe Rosa <flp.far@gmail.com>
2020-03-31 17:59:58 -0300

    Add my_all? and my_any? methods

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/AbcSize
+
 # Adding methods to Enumerable module
 module Enumerable
   def my_each
@@ -21,4 +23,36 @@ module Enumerable
     my_each { |n| array.push(n) if yield(n) }
     array
   end
+
+  def my_all?(arg = nil)
+    if block_given? && arg.nil?
+      my_each { |n| return false unless yield(n) }
+    elsif arg.nil?
+      my_each { |n| return false unless n }
+    elsif arg.is_a?(Regexp)
+      my_each { |n| return false unless arg.match(n) }
+    elsif arg.is_a?(Integer) || arg.is_a?(String)
+      my_each { |n| return false unless n == arg }
+    else
+      my_each { |n| return false unless n.is_a?(arg) }
+    end
+    true
+  end
+
+  def my_any?(arg = nil)
+    if block_given? && arg.nil?
+      my_each { |n| return true if yield(n) }
+    elsif arg.nil?
+      my_each { |n| return true if n }
+    elsif arg.is_a?(Regexp)
+      my_each { |n| return true if arg.match(n) }
+    elsif arg.is_a?(Integer) || arg.is_a?(String)
+      my_each { |n| return true if n == arg }
+    else
+      my_each { |n| return true if n.is_a?(arg) }
+    end
+    false
+  end
 end
+
+# rubocop:enable all
